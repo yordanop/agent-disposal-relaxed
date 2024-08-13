d53982a403648ae9b55bd10881d416066281f16a
Eynar <yordanopv@gmail.com>
2024-08-12 23:13:36 -0600

    added styling from past portfolio

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,42 +1,20 @@
-import { useState, useEffect } from 'react';
-// Bringing in the required component from 'react-router-dom' for linking between pages
-import { Link } from 'react-router-dom';
-import Profile from '../components/UI/ProfileSections/ProfileTeaser';
-import ListItem from '../components/UI/ListItem';
 
-import API from '../utils/API';
+
 
 export default function HomePage() {
-  // Prior to the return statement, our homepage uses a few react hooks and fetchData function to query to a mock database and retrieve random user data
-  const [users, setUsers] = useState([]);
 
-  const fetchData = async () => {
-    const { data } = await API.getUsers();
 
-    setUsers(data);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
 
   // Iterate over each mock user to display their abridged profile data and a link to their page
   return (
-    <div className="container pt-4">
-      <ul className="list-group list-group">
-        {users.map((user) => (
-          <ListItem key={user.id}>
-            <Profile user={user} />
-            {/* Link elements are anchors under-the-hood, but they allow the routing behavior to be controlled by the client rather than the server */}
-            <Link
-              to={`/profile/${user.id}`}
-              className="badge bg-primary rounded-pill"
-            >
-              See More
-            </Link>
-          </ListItem>
-        ))}
-      </ul>
+    <div className="container pt-4 about-me">
+      <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent at sagittis risus. Aliquam vulputate, risus eu pharetra mollis, tellus est maximus ipsum, sit amet pellentesque erat est id risus. Curabitur fermentum nec nibh at pellentesque. Cras eu tempus purus. Vestibulum lacinia, urna ac viverra viverra, urna est fringilla nisi, eget fringilla lacus justo et lectus. Nullam vulputate elit et molestie dictum. Sed dignissim nisi quam. Phasellus varius mattis ligula, et vestibulum libero mollis sit amet. Vestibulum facilisis pretium luctus. Donec luctus eleifend posuere. 
+
+          Morbi pharetra imperdiet commodo. Maecenas aliquet ipsum at hendrerit egestas. In feugiat maximus faucibus. Maecenas laoreet scelerisque suscipit. Integer turpis eros, hendrerit eu tortor et, elementum fermentum ligula. Aenean tempor eros non metus rutrum fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque congue tincidunt est a aliquet. Nam viverra diam ac tortor sollicitudin, ac dapibus leo tempor. Quisque eleifend mattis neque sit amet mollis. Etiam molestie ante ut lacus gravida, tristique porta augue convallis.
+   
+        </p>
+
     </div>
   );
 }
--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,37 +1,77 @@
-import { useState, useEffect } from 'react';
-// Bringing in the required component from 'react-router-dom' for linking between pages and getting the current param variable's value from URL
-import { useParams, Link } from 'react-router-dom';
-import Profile from '../components/UI/ProfileSections/ProfileDetailed';
-import Container from '../components/UI/ListItem';
 
-import API from '../utils/API';
+
+import horiseonImage from '../assets/images/Horiseon-project.png'
+import firstProjectImage from '../assets/images/First_Project.png'
+import secondProjectImage from '../assets/images/Horiseon-project.png'
+import PortfolioImage from '../assets/images/Yordano-portfolio.png'
+import soonImage from '../assets/images/coming-soon.png'
+
+
 
 export default function ProfilePage() {
-  const [user, setUser] = useState({});
 
-  // The useParams hook will yield an object. Its keys match the parameters defined on each route. Its values match the current URL value in those parameter locations
-  const { id } = useParams();
-
-  const fetchData = async () => {
-    const { data } = await API.getSingleUser(id);
-
-    setUser(data);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
 
   return (
-    <>
-      <Container>
-        {/* Conditionally render the full profile or a Loading string, depending on whether user data is available */}
-        {user.name ? <Profile user={user} /> : <p>Loading...</p>}
-      </Container>
-      <footer className="profile-footer">
-        {/* Link the user back to the homepage. The to prop is used in place of an href */}
-        <Link to="/">← Go Back</Link>
-      </footer>
-    </>
+      <div class="content">
+    
+        <div class="imag-cntr first">
+            <a href="https://yordanop.github.io/frost-umbrella-mattress/" target="_blank">
+                <div class="project-title">
+                <p class="s_t">Github : <a href="https://yordanop.github.io/yordano-portfolio/" target="_blank">Here</a></p>
+                </div>
+                <img src={firstProjectImage} alt="preview of fisrt project" class="image-1"/>
+            </a>       
+        </div>
+        
+        <div class="imag-cntr left">
+            <a href="https://yordanop.github.io/yordano-portfolio/" target="_blank">
+                <div class="project-title">
+                    <p class="f_t">My Portfolio</p>
+                    <p class="s_t">HTML and CSS</p>
+                </div>
+                <img src={soonImage} alt="Preview of this portfolio" class="image-2"/>
+            </a>
+        </div>
+
+        <div class="imag-cntr right">
+            <a href="https://yordanop.github.io/payroll-tracker/" target="_blank">
+                <div class="project-title">
+                    <p class="f_t">Pay roll tracker</p>
+                    <p class="s_t">Javascript</p>
+                </div>
+                <img src={soonImage} alt="Preview of payroll" class="image-3"/>
+            </a>
+        </div>
+        
+        <div class="imag-cntr left">
+            <a href="https://yordanop.github.io/frost-umbrella-mattress/" target="_blank">
+                <div class="project-title">
+                    <p class="f_t">First Blog</p>
+                    <p class="s_t">HTML CSS & Javascript</p>
+                </div>
+                <img src={firstProjectImage} alt="Preview of First Blog" class="image-4"/>
+            </a>
+        </div>
+            
+
+        <div class="imag-cntr right">
+            <a href="https://schiffskrieg.onrender.com/" target="_blank">
+                <div class="project-title">
+                    <p class="f_t">Second Project</p>
+                    <p class="s_t">Schieffskrieg</p>
+                </div>
+                <img src={secondProjectImage} alt="Planner preview" class="image-5"/>
+            </a>
+        </div>
+        <div class="imag-cntr right">
+            <a href="https://yordanop.github.io/atoms-await-lizard/" target="_blank">
+                <div class="project-title">
+                    <p class="f_t">First Project</p>
+                    <p class="s_t">Das Wohnzimmer</p>
+                </div>
+                <img src={firstProjectImage} alt="First project preview" class="image-5"/>
+            </a>
+        </div>
+      </div>
   );
 }
